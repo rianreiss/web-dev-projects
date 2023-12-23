200fdfcb6f1c99ef8b0f4578f8c52c0fe16f2852
rianreiss <rianreis2109@gmail.com>
2023-12-23 13:29:30 -0300

    event key added to calc

--- docs/projects/calculator/calc/script.js
+++ docs/projects/calculator/calc/script.js
@@ -1,18 +1,22 @@
 function insert(num) {
+    // insere o item no mercado
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
 
 function clean() {
+    // limpa o display
     document.getElementById('resultado').innerHTML = "";
 }
 
 function back() {
+    // "backspace" apaga 1 item do display
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero.substring(0, numero.length -1);
 }
 
 function calcular() {
+    // faz o calculo com a operação do display
     var numero = document.getElementById('resultado').innerHTML;
 
     if(numero) {
@@ -21,3 +25,21 @@ function calcular() {
         document.getElementById('resultado').innerHTML = "...";
     }
 }
+
+document.addEventListener('keydown', function(event) {
+    // Verifica se a tecla pressionada é um número de 0 a 9
+    if (event.key >= '0' && event.key <= '9') {
+        insert(event.key);
+    } else if (event.key === '.' || event.key === ',') {
+        insert(event.key === ',' ? '.' : event.key);
+    } else if (event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/') {
+        insert(' ' + event.key + ' ');
+    } else if (event.key === 'c' || event.key === 'C') {
+        clean();
+    } else if (event.key === 'Enter') {
+        // Execute a lógica de cálculo quando a tecla Enter for pressionada
+        calcular();
+    } else if (event.key === 'Backspace') {
+        back();
+    }
+});
