ecdf95d8aeb574ded2d114861e1108026cb63938
rianreiss <rianreis2109@gmail.com>
2024-01-04 21:58:36 -0300

    home page finished

---
+++ docs/index/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navbar = document.getElementById('nav');
+  const back = document.getElementById('back');
+  const toggleButton = document.getElementById('toggle-nav');
+
+  // Mostrar/ocultar a barra de navegação ao clicar no botão
+  toggleButton.addEventListener('click', function () {
+    navbar.classList.toggle('hidden');
+  });
+
+  // Ocultar a barra de navegação quando clicar fora dela
+  document.addEventListener('click', function (event) {
+    const isClickInsideNavbar = navbar.contains(event.target);
+    const isClickInsideToggleButton = toggleButton.contains(event.target);
+
+    if (!isClickInsideNavbar && !isClickInsideToggleButton) {
+      navbar.classList.add('hidden');
+    }
+  });
+
+  back.addEventListener('click', function () {
+    window.location.href = 'index.html';
+  });
+
+});
