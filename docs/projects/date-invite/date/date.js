3a1339b2869c63197d5ae07487e8c2724fd188b5
rianreiss <rianreis2109@gmail.com>
2024-01-07 18:05:14 -0300

    date invite project media query and js updates

--- docs/projects/date-invite/date/date.js
+++ docs/projects/date-invite/date/date.js
@@ -16,8 +16,8 @@ function dodge(t) {
     var btn = t;
 
     btn.style.position = 'absolute';
-    btn.style.bottom = posicaoAleatoria(10, 85);
-    btn.style.left = posicaoAleatoria(15, 85);
+    btn.style.bottom = posicaoAleatoria(10, 80);
+    btn.style.left = posicaoAleatoria(15, 70);
 }
 
 function posicaoAleatoria(min, max) {
